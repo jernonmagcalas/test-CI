2e090babd4ec98c178085a3e0543dd53619c2c10
Jernon Magcalas <jernonmagcalas@gmail.com>
2015-09-23 21:07:52 +0800

    remove test setname

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,8 @@ if[ $(TRAVIS_BRANCH) = 'staging' ] && (exit 0;)
 git config --global user.email = 'jernonmagcalas@gmail.com'
 git config --global user.name = 'jernon magcalas'
 
-echo "$TRAVIS_REPO_SLUG"
+git remove -v
+
 git remote add repoo https://jernonmagcalas:shylentnyt01@github.com/$TRAVIS_REPO_SLUG.git
 git fetch repoo
 git stash
