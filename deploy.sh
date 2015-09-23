bae53a7cca5e609737f4eba3a2d666a37ec6788d
Jernon Magcalas <jernonmagcalas@gmail.com>
2015-09-23 19:31:12 +0800

    remove test setname

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ if[ $TRAVIS_BRANCH = 'staging' ]; then
 fi
 
 echo "$TRAVIS_REPO_SLUG"
-#git remote add repoo https://$username:$password@github.com/$TRAVIS_REPO_SLUG.git
+git remote add repoo https://$username:$password@github.com/$TRAVIS_REPO_SLUG.git
 #git fetch repoo
 git checkout repoo/staging
 git merge $TRAVIS_COMMIT
