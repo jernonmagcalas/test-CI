b802cfe3d9b7e2517e31e2c4f54a8c562a1eaa16
Jernon Magcalas <jernonmagcalas@gmail.com>
2015-09-23 19:03:11 +0800

    remove test setname

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 echo "$TRAVIS_REPO_SLUG"
 git remote add repoo https://github.com/$TRAVIS_REPO_SLUG.git
 git fetch repoo
-git checkout repoo master
-git merge repoo staging
+git checkout repoo/master
+git merge repoo/staging
 git push repoo master
