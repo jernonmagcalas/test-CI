37bb9471046cb3ac0c86b83b4f2e6868460bf593
Jernon Magcalas <jernonmagcalas@gmail.com>
2015-09-23 13:20:41 +0800

    initial test

---
+++ Source/autoload.php
@@ -0,0 +1,3 @@
+<?php
+require('Person.php');
+require('Human.php');
---
+++ Test/HumanTest.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: SaTuRn
+ * Date: 9/23/2015
+ * Time: 1:05 PM
+ */
+
+namespace Test;
+
+
+use Source\Human;
+
+class HumanTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testInstance()
+    {
+        $human = new Human('clark');
+
+        $this->assertInstanceOf('Source\Person', $human);
+    }
+}
