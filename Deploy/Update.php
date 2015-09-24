b5374429c47820bbbef6a5b1b773e071ce5fc8e3
Jernon Magcalas <jernonmagcalas@gmail.com>
2015-09-24 13:26:29 +0800

    added auto update

--- Deploy/Update.php
+++ Deploy/Update.php
@@ -2,7 +2,7 @@
 $header = apache_request_headers();
 //verify the request
 $secret = 'secret_key'; // you set this in the web hook settings
-$signature = hash_hmac('sha1', $_POST, $$secret, true);
+$signature = 'sha1='.hash_hmac('sha1', $_POST, $$secret, true);
 if (empty($_POST)
     || !isset($header['HTTP_X_HUB_SIGNATURE'])
     || $header['HTTP_X_HUB_SIGNATURE'] != $signature
