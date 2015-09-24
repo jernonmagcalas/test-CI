166f0800943462a0a4b14016a2e53b11e2e1afed
Jernon Magcalas <jernonmagcalas@gmail.com>
2015-09-24 13:08:19 +0800

    added auto update

--- Deploy/Update.php
+++ Deploy/Update.php
@@ -1,7 +1,8 @@
 <?php
 $header = apache_request_headers();
 //verify the request
-$signature = 'your signature here';
+$secret = 'secret_key'; // you set this in the web hook settings
+$signature = hash_hmac('sha1', $_POST, $$secret, true);
 if (empty($_POST)
     || !isset($header['HTTP_X_HUB_SIGNATURE'])
     || $header['HTTP_X_HUB_SIGNATURE'] != $signature
@@ -18,5 +19,3 @@ if ($_POST['repository']['name'] == 'staging') {
 if ($_POST['repository']['name'] == 'master') {
     exec('git pull origin master');
 }
-
-//file_put_contents(json_decode($_POST));
